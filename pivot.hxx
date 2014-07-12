44a8448b200f650b3c4d76016a07ff22fcf4f5f7
Lennard Wasserthal <Wasserthal@nefkom.net>
2014-07-12 21:22:59 +0200

    Other elements are spinning
    The graphic object rushes around like mad because it has no undo buffer to reset it.
    
    Signed-off-by: Lennard Wasserthal <Wasserthal@nefkom.net>

--- pivot.hxx
+++ pivot.hxx
@@ -24,6 +24,10 @@ void get_selection_pivot()
 }
 void applytransform(float matrix[3][3])
 {
+	intl ioffset;
+	float tl_x,tl_y;
+	char * ibufferpos;
+	_u32 icompare;
 	cdx_Point3D xyz;
 	for (int ilv1=0;ilv1<(*glob_n_multilist).filllevel;ilv1++)
 	{
@@ -40,6 +44,33 @@ void applytransform(float matrix[3][3])
 				(*tl_n_instance).xyz.z=xyz.x*matrix[0][2]+xyz.y*matrix[1][2]+xyz.z*matrix[2][2]+edit_pivot.z;
 			}
 		}
+	}
+	for (int ilv1=2;ilv1<STRUCTURE_OBJECTTYPE_ListSize;ilv1++)
+	{
+		icompare=1<<ilv1;
+		int isize= STRUCTURE_OBJECTTYPE_List[ilv1].size;
+		basicmultilist * tlmultilist=findmultilist(STRUCTURE_OBJECTTYPE_List[ilv1].name);
+		if (tlmultilist==NULL) goto i_standard_fertig;
+		CDXMLREAD_functype tldummy;
+		ibufferpos=(char*)((*tlmultilist).pointer);
+		for (int ilv2=0;ilv2<(*tlmultilist).filllevel;ilv2++)
+		{
+			if ((selection_currentselection[ilv2]) & icompare)
+			{
+				if ((*((basic_instance*)(ibufferpos+isize*ilv2))).exist)
+				{
+					retrievepoints_basic(((basic_instance*)(ibufferpos+isize*ilv2)),&tl_x,&tl_y,0,ilv1);
+					xyz.x=tl_x-edit_pivot.x;
+					xyz.y=tl_y-edit_pivot.y;
+					xyz.z=0-edit_pivot.z;//TODO: tl_z
+					tl_x=xyz.x*matrix[0][0]+xyz.y*matrix[1][0]+xyz.z*matrix[2][0]+edit_pivot.x;
+					tl_y=xyz.x*matrix[0][1]+xyz.y*matrix[1][1]+xyz.z*matrix[2][1]+edit_pivot.y;
+					//TODO: tl_z
+					placepoints_basic(((basic_instance*)(ibufferpos+isize*ilv2)),tl_x,tl_y,0,ilv1);
+				}
+			}
+		}
+		i_standard_fertig:;
 	}
 	return;
 }

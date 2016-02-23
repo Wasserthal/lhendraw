aeb9676c496ba0a12c78dfc99abd649c6d18b19a
Lennard Wasserthal <Wasserthal@nefkom.net>
2016-02-23 17:23:54 +0000

    Help menu
    Signed-off-by: Lennard Wasserthal <Wasserthal@nefkom.net>

--- help.hxx
+++ help.hxx
@@ -1,4 +1,5 @@
-char helpstring[]="Usage\n"
+char help_menu[]="HELP MENU\n\nPress the corresponding number key to select topic\n\n1: Introduction\n2: other tools\n3: command line\n\npress ESCAPE to return to program";
+char help_introduction[]="Usage\n"
 "The first thing you do is to use the \n\n\e11  tool.\nDraw a carbon chain (which contains implicit hydrogens by default).\n"
 "If you want to draw other elements, use the \n\n\e12  tool.\nSelect an atom or more, then click this tool. A periodic table appears.\n"
 "click any Element there and the selected atoms will be that element.\n"
@@ -13,16 +14,37 @@ char helpstring[]="Usage\n"
 "You draw the arrow from start to end.\n"
 "If you want to bend them, use the arrow -\"Skipping\" tool \e72 .\n"
 "which can be selected from arrows mode.";
+char help_othertools[]="OTHER TOOLS\n\n"
+"When a tool is selected, right of the main display,\nconfiguration parameters for that tool appear.\n\n"
+"If you are in selection mode, by means of the \e9  or \e6  tool,\n"
+"you can use the right mouse button to drag the selected shapes.\n\n"
+"If you want to modify an atom, use the \e35  tool.";
+char help_commandline[]="COMMANDLINE\n\nAdding filenames to the lhendraw command will open them.\nAdding several Filenames will drop the contents on top of each other.\nYou can also use the command line to search for files containing a certain structure.\nTo do this, enter\nlhendraw searchpattern.cdxml --search files_to_search.cdxml\nTo get full information about the command-line functions, enter lhendraw -h";
+char * helpstring=help_introduction;
 void printmenutext(int posx,int posy,const char * iinput,int end,char symbolmode=0);
 void control_help()
 {
 	SDL_Event control_Event;
+	helpstring=help_menu;
 	iback:
 	SDL_PollEvent(&control_Event);
-	if (control_Event.type==SDL_KEYDOWN) return;
+	if (control_Event.type==SDL_KEYDOWN)
+	{
+		switch (control_Event.key.keysym.sym)
+		{
+			case SDLK_F1:
+			case SDLK_1: helpstring=help_introduction;break;
+			case SDLK_2: helpstring=help_othertools;break;
+			case SDLK_3: helpstring=help_commandline;break;
+			case SDLK_m: helpstring=help_menu;break;
+			default:
+			return;
+		}
+	}
+	if (control_Event.type==SDL_QUIT) return;
 	gfx_gfxstart();
 	screenclear(0xFFFFFF);
-	printmenutext(0,20,helpstring,sizeof(helpstring)-1,1);
+	printmenutext(0,20,helpstring,strlen(helpstring),1);
 	gfx_gfxstop();
 	goto iback;
 }

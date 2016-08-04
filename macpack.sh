4d5073ec4b9725064a0a74a4b6b634e50f30228f
Lennard Wasserthal <Wasserthal@nefkom.net>
2016-08-04 21:25:32 +0200

    script for MAC OsX executable(draft)
    Signed-off-by: Lennard Wasserthal <Wasserthal@nefkom.net>

---
+++ macpack.sh
@@ -0,0 +1,9 @@
+mkdir lhendraw.app
+mkdir lhendraw.app/Contents
+mkdir lhendraw.app/Contents/MacOS
+mkdir lhendraw.app/gfx
+cp ilhendraw lhendraw.app/Contents/MacOS/lhendraw
+cp gfx/buttons.bmp lhendraw.app/buttons.bmp
+cp LiberationMono-Regular.lennardfont lhendraw.app
+cp LiberationMono-Regular.bin lhendraw.app
+cp hotkeys.xml lhendraw.app

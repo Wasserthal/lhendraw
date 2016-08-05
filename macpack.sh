7875093fe32e26bbfb13cc5b5cdc2ff319fd91f0
Lennard Wasserthal <Wasserthal@nefkom.net>
2016-08-05 14:17:16 +0200

    Added icon to Mac OSX
    Signed-off-by: Lennard Wasserthal <Wasserthal@nefkom.net>

--- macpack.sh
+++ macpack.sh
@@ -1,9 +1,12 @@
 mkdir lhendraw.app
 mkdir lhendraw.app/Contents
 mkdir lhendraw.app/Contents/MacOS
+mkdir lhendraw.app/Contents/Resources
 mkdir lhendraw.app/gfx
-cp ilhendraw lhendraw.app/Contents/MacOS/lhendraw
-cp gfx/buttons.bmp lhendraw.app/buttons.bmp
+cp build/Debug/ilhendraw lhendraw.app/Contents/MacOS/lhendraw
+cp gfx/buttons.bmp lhendraw.app/gfx/buttons.bmp
 cp LiberationMono-Regular.lennardfont lhendraw.app
 cp LiberationMono-Regular.bin lhendraw.app
 cp hotkeys.xml lhendraw.app
+cp Info.plist lhendraw.app/Contents
+cp lhendraw.icns lhendraw.app/Contents/Resources
